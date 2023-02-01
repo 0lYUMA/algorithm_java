cc267730808be882ec064b2e19a9169cffd1ecb8
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-02-01 19:23:13 +0900

    Create SubagSubagSu.java
    수박수박수박수박수박수?

---
+++ Level_1/SubagSubagSu.java
@@ -0,0 +1,16 @@
+package Level_1;
+
+public class SubagSubagSu {
+    public String main(int n) {
+        String answer = "";
+        
+        for (int i = 0; i < n / 2; i++)
+        {
+            answer += "수박";
+        }
+        
+        if (n % 2 == 1) answer += "수";
+        
+        return answer;
+    }
+}
