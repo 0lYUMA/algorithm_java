2fc2fb4c6a0858e6ed57d80b6f4744316e65d0a4
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-02-03 15:18:30 +0900

    Create MeasureNumPlus.java
    약수의 개수와 덧셈

---
+++ Level_1/MeasureNumPlus.java
@@ -0,0 +1,25 @@
+package Level_1;
+
+public class MeasureNumPlus {
+    public int main(int left, int right) {
+        int answer = 0;
+        
+        for (int i = left; i <= right; i++)
+        {
+            int measure = 0;
+            
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    measure++;
+                }
+            }
+
+            if (measure % 2 == 0) answer += i;
+            else answer -= i;
+        }
+        
+        return answer;
+    }
+}
