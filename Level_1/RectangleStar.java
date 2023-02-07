9b7b6f557db5c87b5586ad339b4d5eea6d120b61
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-02-08 01:43:38 +0900

    Create RectangleStar.java
    직사각형 별 찍기

---
+++ Level_1/RectangleStar.java
@@ -0,0 +1,23 @@
+package Level_1;
+
+import java.util.Scanner;
+
+public class RectangleStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String star = "";
+        
+        for (int i = 0; i < b; i++)
+        {
+            for (int j = 0; j < a; j++)
+            {
+                star += "*";
+            }
+            star += "\n";
+        }
+        
+        System.out.println(star);
+    }
+}
